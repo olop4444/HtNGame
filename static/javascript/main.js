bdccd1ec3272fa3841dfab5fcd2803c4c68313a9
David Song <David.Song0802@gmail.com>
2015-09-19 05:20:45 -0400

    added pixi.js to game; initialized renderer and stage

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,1 +1,21 @@
-console.log('Hello world. Earth to world. Come in world.');
+// main.js
+//  -- setting up canvas
+
+// Pixi JS renderer instance
+var renderer = PIXI.autoDetectRenderer(800, 600);
+
+// Add renderer into DOM tree
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Stage(0x66FF99);
+
+requestAnimationFrame(animate);
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	// console.log('animate ran');
+
+	// render the stage
+	renderer.render(stage);
+}

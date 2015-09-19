cab65dca452efde3d40a8845f6683c333d387929
David Song <David.Song0802@gmail.com>
2015-09-19 12:37:54 -0400

    add canvas.html to server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(3000);
+app.listen(80);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+function handler(req, res) {
+  fs.readFile(__dirname + '/static/canvas.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);

af5be2a1b40f93b63e5dbe2eea5883d944c8c68e
David Song <david.song0802@gmail.com>
2015-09-19 16:23:03 +0000

    (from aws) packaged as npm; dependencies should settle

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(820);
+app.listen(3000);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
